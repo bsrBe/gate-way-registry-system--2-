ca8b609c21ee7ed252e649f378e7818b10c11796
Bisrat Abrham <bsr7349@gmail.com>
2025-06-17 10:28:36 +0300

    Update apiSlice.js

--- Main-Application/src/slices/apiSlice.js
+++ Main-Application/src/slices/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 
-const baseQuery = fetchBaseQuery({ baseUrl: "https://gatewayregistry.onrender.com", credentials: "include" })
-
+// const baseQuery = fetchBaseQuery({ baseUrl: "https://gatewayregistry.onrender.com", credentials: "include" })
+const baseQuery = fetchBaseQuery({ baseUrl: "https://grs-gbeb.onrender.com", credentials: "include" })
 
 export const apiSlice = createApi({
     baseQuery,
